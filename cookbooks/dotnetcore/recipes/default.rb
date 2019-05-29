94956dff3e8c6a16645e6640cdfcc94bb51eaad8
Jesus Sanchez <jesus-alejandro.sanchez-davila@massive.se>
2019-05-29 09:57:01 +0200

    [WIP] In the middle of something

--- cookbooks/dotnetcore/recipes/default.rb
+++ cookbooks/dotnetcore/recipes/default.rb
@@ -5,11 +5,43 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 #
 
-if node['platform_family'] == 'rhel' do
-    case node['platform']
-    when 'centos' do
-        
+if node['os'] == 'linux' do
+    case node['platform_family']
+    when 'rhel' do
+        case node['platform']
+        when 'centos','oracle' do
+            remote_file "#{Chef::Config[:file_cache_path]}/packages-microsoft-prod.rpm" do
+                source "https://packages.microsoft.com/config/rhel/<%= node.platform_version.split('.')[0] %>/packages-microsoft-prod.rpm"
+                action :create
+            end        
+            rpm_package "ms-repo-prod-rpm" do
+                source "#{Chef::Config[:file_cache_path]}/packages-microsoft-prod.rpm"
+                action :install
+                notifies :run, 'execute[update-yum-cache]', :immediate
+            end
+            execute 'update-yum-cache' do
+                command 'yum makecache -y'
+                action :nothing
+            end
+        end
     end
-elsif node['platform_family'] ==  'debian' do
-elsif node['platform_family'] ==  'windows' do
+    when  'debian' do
+        case node['platform']
+        when 'ubuntu' do
+            remote_file "#{Chef::Config[:file_cache_path]}/packages-microsoft-prod.rpm" do
+            source "https://packages.microsoft.com/config/ubuntu/<%= node.platform_version %>/packages-microsoft-prod.deb"
+            action :create
+        end        
+        dpkg_package "ms-repo-prod-deb" do
+            source "#{Chef::Config[:file_cache_path]}/packages-microsoft-prod.deb"
+            action :install
+            notifies :run, 'execute[update-apt-cache]', :immediate
+        end
+        execute 'update-apt-cache' do
+            command 'apt-get update -y'
+            action :nothing
+        end
+        when 'debian' do
+        end
+    end
 end
--- cookbooks/dotnetcore/spec/unit/recipes/default_spec.rb
+++ cookbooks/dotnetcore/spec/unit/recipes/default_spec.rb
@@ -7,20 +7,7 @@
 require 'spec_helper'
 
 describe 'dotnetcore::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
+  context 'CentOS' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
@@ -31,5 +18,26 @@ describe 'dotnetcore::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
+
+    it 'installs dotnet core packages' do
+      expect { chef_run }.to install_package('dotnetcore')
+    end
   end
+
+  context 'Ubuntu' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '18.04')
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+    it 'installs dotnet core packages' do
+      expect { chef_run }.to install_package('dotnetcore')
+    end
+  end
+
 end
