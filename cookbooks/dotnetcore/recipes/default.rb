077547853a3b44248c72b25efdd61965a5716700
Jesus Alejandro Sanchez Davila <silvarion@gmail.com>
2019-05-29 12:23:52 +0000

    Modified version to 2.1

--- cookbooks/dotnetcore/recipes/default.rb
+++ cookbooks/dotnetcore/recipes/default.rb
@@ -77,8 +77,7 @@ if node['os'] == 'linux'
         end
     end
     # Every Distro
-    package 'dotnet-core' do
-        name 'dotnet-sdk-2.2'
+    package 'dotnet-sdk-2.1' do
         action :install
     end
 end
